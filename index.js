fc2dab5fd4ebba5b4be1322cb0d338ea9b047525
Nir Barzilay <nbtk123@gmail.com>
2016-12-10 19:50:28 +0200

    fetching items from mongoDB

--- index.js
+++ index.js
@@ -12,15 +12,19 @@ var url = 'mongodb://user:user@ds129038.mlab.com:29038/reminder_by_url';
 app.get('/', function(request, response) {
 	// Use connect method to connect to the server
 	MongoClient.connect(url, function(err, db) {
-	  assert.equal(null, err);
-	  console.log("Connected successfully to server");
-	  db.close();
-	  response.send('Connected successfully to server');
+		assert.equal(null, err);
+		console.log("Connected successfully to server");
+	  
+		var collection = db.collection('nbtk123');
+		collection.find({}).toArray(function(err, docs) {
+			assert.equal(err, null);
+			
+			db.close();
+			response.send(docs[0]);
+		});
 	});
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
