29904b1698857335d6ecf399f8fda4e5ffa7540a
Nir Barzilay <nbtk123@gmail.com>
2016-12-10 19:21:50 +0200

    applying heroku

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
